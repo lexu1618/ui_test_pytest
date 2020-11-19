6949f2dc51473dd1bbec2e4178abc42506af1cb5
lexus1618 <399184356@qq.com>
2020-11-19 23:08:17 +0800

    q

--- TestCases/demo_test/te_1.py
+++ TestCases/demo_test/te_1.py
@@ -4,10 +4,10 @@ import pytest
 login_format_data =[{'username': '13691579846', 'password': 'xiaochao1152', 'expected': '帐号或密码错误!'},\
                     {'username': '13691579846', 'password': 'xiaochao115200', 'expected': '帐号或密码错误!'}]
 
-# @pytest.mark.parametrize("a,b,c",login_format_data)
-# def test_a(a,b,c):
-#     print(a,b,c)
-#     print(type(a))
+@pytest.mark.parametrize("a,b,c",login_format_data)
+def test_a(a,b,c):
+    print(a,b,c)
+    print(type(a))
 
 @pytest.mark.parametrize("data",login_format_data)
 def test_a(data):
